4b8550f7541436e3775a512f2755211b4d37be17
G. Carolina Hernandez <carolinahrndez3@gmail.com>
2023-07-11 11:04:57 +0100

    create an event form working

--- components/events/EventForm.jsx
+++ components/events/EventForm.jsx
@@ -1,23 +1,108 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import { CreateEventAPI } from '../../services/event.service'
+import ErrorMsg from '../common/ErrorMsg'
+import { XMark } from '../common/Icons'
 
 function EventForm({ handleForm }) {
+  // DATA
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
+  const [errorMsg, setErrorMsg] = useState('')
+  const [showError, setShowError] = useState(false)
+
+  // TITLE
+  const handleTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const validationTitle = () => {
+    return (title.length > 4)
+  }
+
+  // DATE
+  const handleDate = (e) => {
+    setDate(e.target.value)
+  }
+
+  const validationDate = () => {
+    const today = new Date();
+    const event = new Date(date)
+
+    return (event > today)
+  }
+
+  // START
+  const handleStart = (e) => {
+    setStart(e.target.value)
+  }
+
+  // END
+  const handleEnd = (e) => {
+    setEnd(e.target.value)
+  }
+
+  const validationEnd = () => {
+    return (start < end)
+  }
+
+  // CREATE EVENT SERVICE
+  const CreateEventService = async () => {
+    const res = await CreateEventAPI(title, date, start, end)
+    console.log(res)
+    if (res) {
+      window.location.href = window.location.href
+    }
+  }
+
+  // ERROR 
+  const showErrorMsg = () => {
+    setShowError(true)
+    setTimeout(() => { setShowError(false) }, 4000);
+  }
+
+  const hideErrorMsg = () => {
+    setShowError(false)
+  }
+
+  // SUBMIT
+  function submitForm(e) {
+    e.preventDefault();
+    if (
+      validationTitle() &&
+      validationDate() &&
+      validationEnd()
+    ) {
+      CreateEventService()
+    } else {
+      setErrorMsg('Warning! Some fields are incorrect or empty')
+      showErrorMsg()
+    }
+  }
+
   return (
-    <div className="rounded-lg bg-white border transform translate-x-[-50%] left-[50%] top-10 sm:top-[50%] absolute border-gray-300 p-5 w-11/12 lg:w-2/5 xl:w-1/5 py-[50px] px-10 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] ">
+    <div className="rounded-lg bg-white border transform translate-x-[-50%] left-[50%] top-10 sm:top-[40%] absolute border-gray-300 p-5 w-11/12 lg:w-2/5 xl:w-1/5 py-[50px] px-10 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] ">
       <div onClick={handleForm} className="bg-white cursor-pointer rounded-md p-2 inline-flex absolute items-center justify-center top-4 right-4 text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
         <span className="sr-only">Close menu</span>
-        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-        </svg>
+        <XMark className='h-6 w-6' />
       </div>
       <h1 className="text-3xl font-medium">New event...</h1>
       <p className="text-sm">Just need some details to start</p>
-      <form className="space-y-5 mt-5">
-        <input type="text" className="w-full h-12 border border-gray-800 rounded px-3" placeholder="Title*" />
-        <input type="text" className="w-full h-12 border border-gray-800 rounded px-3" placeholder="Date*" />
+      <form className="space-y-3 mt-5" onSubmit={(e) => submitForm(e)}>
+        <input type="text" className="w-full h-12 border border-gray-800 rounded px-3" placeholder="Title*" onChange={handleTitle} />
+        <span className={`m-0 p-0 ${!validationTitle() && title !== '' ? 'visible' : 'hidden'} text-red-600 text-xs`}>Please provide a valid title.</span>
+        <input className="w-full h-12 border border-gray-800 rounded px-3" type='date' placeholder="Date*" onChange={handleDate}/>
+        <span className={`m-0 p-0 ${!validationDate() && date !== '' ? 'visible' : 'hidden'} text-red-600 text-xs`}>Please provide a valid date.</span>
         <div className="flex gap-5">
-          <input className="w-full h-12 border border-gray-800 rounded px-3" placeholder="Start time*" />
-          <input className="w-full h-12 border border-gray-800 rounded px-3" placeholder="End time*" />
+          <input className="w-full h-12 border border-gray-800 rounded px-3" type='time' placeholder="Start time*" onChange={handleStart}/>
+          <input className="w-full h-12 border border-gray-800 rounded px-3" type='time' placeholder="End time*"  onChange={handleEnd}/>
         </div>
+        <span className={`m-0 p-0 ${!validationEnd() && end !== '' ? 'visible' : 'hidden'} text-red-600 text-xs`}>End time must be after start time.</span>
+        {showError &&
+          <ErrorMsg message={errorMsg} hide={hideErrorMsg}/>
+        }
         <button className="text-center w-full bg-secondary bg-opacity-70 rounded-lg py-3 font-medium" type='submit'>Start</button>
       </form>
     </div>
--- services/event.service.js
+++ services/event.service.js
@@ -12,3 +12,17 @@ export const GetEventsAPI = async () => {
     console.error('Cannot get events', error)
   }
 }
+
+export const CreateEventAPI = async (title, event_date, start_time, end_time) => {
+  try {
+    const { data } = await api.post('/api/events', { title, event_date, start_time, end_time }, {
+        headers: {
+        token: localStorage.getItem('token')
+      }
+    })
+    console.log({data })
+    return data
+  } catch (error) {
+    console.error('Cannot create event', error)
+  }
+}
